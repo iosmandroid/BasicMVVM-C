9db161c27173c211ebf28ee0c9602cafc21e077e
iosmandroid <osmanyildiz046@gmail.com>
2025-06-15 18:22:40 +0300

    initial docs struct

---
+++ BasicMVVM-C/Modules/SendPic/SendPicViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SendPicViewController.swift
+//  BasicMVVM-C
+//
+//  Created by otto on 15.06.2025.
+//
+
+import UIKit
+
+class SendPicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
